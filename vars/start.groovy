cf710cd0b26a22ed16903afea7edaf20c44998a4
zt <zt>
2020-05-25 19:10:04 +0200

    Added start.groovy

---
+++ vars/start.groovy
@@ -0,0 +1,78 @@
+def call(Map args) {
+    agent any
+
+    stages {
+        stage ('start') {
+            steps {
+                script {
+                    branch = "${JOB_BASE_NAME}"
+
+                    sh """
+                        echo 'simple echo'
+                        echo ${JOB_NAME}
+                      
+                
+                        #stuff
+                        branch=${branch}
+                        echo $branch
+
+                    """
+                }
+            }
+        }
+        stage ('Get branch') {
+            steps {
+                script {
+                    sh """
+                        echo "${JOB_BASE_NAME}" | sed 's/\\%2F/\\//g' > /tmp/file.txt
+                        cat /tmp/file.txt
+                    """
+                }
+            }
+        }
+        stage ('Dev branch check') {
+            steps {
+                script {
+                    def git_branch = readFile("/tmp/file.txt").trim()
+                    
+                    if (git_branch == "develop") {
+                        echo "develop branch - do stuff"
+                        args.develop = true
+                    }else{
+                        echo "skipping"
+                    }
+                }
+            }
+        }
+        stage ('Non Dev stuff') {
+            when {
+                expression {
+                    !args.develop
+                }
+            }
+            steps {
+                script {
+                    def git_branch = readFile("/tmp/file.txt").trim()
+
+                    if (git_branch == "develop") {
+                        return
+                    }else{
+                        echo "do more stuff"
+                    }
+                }
+            }
+        }
+        stage ('Master stuff') {
+            when {
+                expression {
+                    !args.develop
+                }
+            }
+            steps {
+                script {
+                    echo "hold my stuff"
+                }
+            }
+        }
+    }
+}
